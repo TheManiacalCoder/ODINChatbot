bb5ac86f89ba6349cc36eb6eaa031767de2c2d72
TheManiacalCoder <sean.m.craig02@gmail.com>
2024-12-31 11:41:53 -0700

    Delete Odin directory

--- Odin/config.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-config_path = os.path.join(os.path.dirname(__file__), 'config.json')
-with open(config_path, 'r', encoding='utf-8') as config_file:
-    config = json.load(config_file)
-
-OPEN_ROUTER_API_KEY = config['OPEN_ROUTER_API_KEY']
-MODEL_NAME = config['MODEL_NAME']
