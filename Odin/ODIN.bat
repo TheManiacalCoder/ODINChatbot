b05bbbca1842c154eddde5e9228d948495e10c60
TheManiacalCoder <sean.m.craig02@gmail.com>
2025-01-01 22:14:59 -0700

    Delete Odin directory

--- Odin/ODIN.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python engine.py
-pause
--- Odin/gui/app.py
+++
@@ -1,5 +0,0 @@
-from .chatbot_ui import ChatbotUI
-
-def run_gui_wrapper(container):
-    chatbot_ui = ChatbotUI(container)
-    return chatbot_ui  # Ensure this line is present
